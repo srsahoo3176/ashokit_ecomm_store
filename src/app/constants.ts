4788f1e3e1462733cc937caf7d302603772c0d12
srsahoo3176 <soumyaranjan3176@gmail.com>
2024-11-12 17:48:46 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://add09b7a58f27428fa2e65f351bf34fb-1956360806.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a08471ea053374aa287c4d6583ec146c-2022332368.ap-south-1.elb.amazonaws.com',
 };
